13b76b9c2b1a15b97205fafb5c2a6293a84c2c3f
hafizuddinraju <hafizraju.cse.bubt@gmail.com>
2022-09-21 06:19:44 +0600

    css added

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Country.css'
 const Country = (props) => {
     return (
-        <div>
+        <div className='country'>
             <img src={props.country.flags.png} alt="" />
             <h2>{props.country.name.common}</h2>
             
